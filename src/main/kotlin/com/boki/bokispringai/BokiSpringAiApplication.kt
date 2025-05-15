b591b2be38efdeda06959515eb86b35cee95a44d
boki <lsb530@naver.com>
2025-05-15 11:32:12 +0900

    init project

---
+++ src/main/kotlin/com/boki/bokispringai/BokiSpringAiApplication.kt
@@ -0,0 +1,11 @@
+package com.boki.bokispringai
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BokiSpringAiApplication
+
+fun main(args: Array<String>) {
+    runApplication<BokiSpringAiApplication>(*args)
+}
---
+++ src/test/kotlin/com/boki/bokispringai/BokiSpringAiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.boki.bokispringai
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BokiSpringAiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
